3a8eb5d0778b271c5ce5ec2d962656101a84aef9
hechmi06 <130917873+hechmi06@users.noreply.github.com>
2023-11-07 19:33:26 +0100

    Add files via upload

---
+++ view/orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName: 'Gafsa Event',
+        productNumber: '85743',
+        paymentStatus: 'Due',
+        status: 'Pending'
+    },
+    {
+        productName: 'YoGa',
+        productNumber: '97245',
+        paymentStatus: 'Refunded',
+        status: 'Declined'
+    },
+    {
+        productName: 'yarhem khalil',
+        productNumber: '36452',
+        paymentStatus: 'Paid',
+        status: 'Active'
+    },
+]
